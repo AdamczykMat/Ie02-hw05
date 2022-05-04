585dcb2bf2b98a9afecc67a7423ded6a811349e5
AdamczykMat <a.mateusz470@gmail.com>
2022-05-04 21:45:56 +0200

    Character movement added

--- Homework5/main.cpp
+++ Homework5/main.cpp
@@ -6,24 +6,19 @@
 #include <SFML/Graphics.hpp>
 
 
-class CustomRectangleShape : public sf::RectangleShape {
+class CustomSprite : public sf::Sprite {
 public:
-    CustomRectangleShape(const sf::Vector2f &size, const sf::Vector2f &position) : sf::RectangleShape(size)
-    {
-        setPosition(position);
-    }
+    CustomSprite(){}
 
-    void setSpeed(const int& x_speed, const int& y_speed, const int & ro_speed) {
+    void setSpeed(const int& x_speed, const int& y_speed) {
         x_speed_ = x_speed;
         y_speed_ = y_speed;
-        ro_speed_ = ro_speed;
     }
 
     void animate(const sf::Time &elapsed){
         bouncce();
         float dt = elapsed.asSeconds();
         move(x_speed_*dt,y_speed_*dt);
-        rotate(ro_speed_*dt);
     }
 
     void setBounds(const float& l_bound, const float& r_bound,const float& u_bound,const float& d_bound){
@@ -36,7 +31,6 @@ public:
 private:
     int x_speed_ = 0 ;
     int y_speed_ = 0 ;
-    int ro_speed_ = 0 ;
     float l_bound_ = 0;
     float r_bound_ = 0;
     float u_bound_ = 0;
@@ -45,27 +39,20 @@ private:
 
     void bouncce(){
         sf::FloatRect rectangle_bounds = getGlobalBounds();
+        std::cout << rectangle_bounds.top << " / " << rectangle_bounds.left << std::endl;
 
-        if(rectangle_bounds.top <= u_bound_){
-            y_speed_ = abs(y_speed_);
-        }
+        if(rectangle_bounds.top <= u_bound_)
+        {y_speed_ = abs(y_speed_);}
 
-        if(rectangle_bounds.top + rectangle_bounds.height >= d_bound_){
-            y_speed_ = abs(y_speed_) * -1;
-            setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
-        }
+        if(rectangle_bounds.top + rectangle_bounds.height >= d_bound_)
+        {y_speed_ = abs(y_speed_) * -1;}
 
-        if(rectangle_bounds.left <= l_bound_ ){
-           x_speed_ = abs(x_speed_);
-           setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
-        }
+        if(rectangle_bounds.left <= l_bound_ )
+        {x_speed_ = abs(x_speed_);}
 
-        if(rectangle_bounds.left + rectangle_bounds.width >= r_bound_){
-            x_speed_ = abs(x_speed_) * -1;
-            setFillColor(sf::Color(rand() % 256, rand() % 256, rand() % 256));
-        }
+        if(rectangle_bounds.left + rectangle_bounds.width >= r_bound_)
+        {x_speed_ = abs(x_speed_) * -1;}
     }
-
 
 };
 
@@ -82,12 +69,6 @@ int main() {
 
     std::vector<std::unique_ptr<sf::Drawable>> shapes;
     create_shapes(shapes);
-    //--------
-    sf::Vector2f size(120.0, 60.0);
-    sf::Vector2f position(120.0, 60.0);
-    CustomRectangleShape rectangle_inh(size, position);
-    rectangle_inh.setFillColor(sf::Color(150, 100, 50));
-    rectangle_inh.setSpeed(100, 150, 10);
 
     sf::Texture texture_bg;
     sf::Texture texture_guy;
@@ -97,16 +78,16 @@ int main() {
     if(!texture_guy.loadFromFile("guy.png")) {std::cerr << "Could not load texture" << std::endl; }//return 0;
     if(!texture_wall.loadFromFile("wall.png")) {std::cerr << "Could not load texture" << std::endl;}// return 0;
 
-    sf::Sprite sprite_bg;
 
     texture_bg.setRepeated(true);
-
+    sf::Sprite sprite_bg;
     sprite_bg.setTexture(texture_bg);
     sprite_bg.setTextureRect(sf::IntRect(0, 0, window.getSize().x, window.getSize().y));
 
 
 
-    sf::Sprite guy;
+    CustomSprite guy;
+    guy.setPosition(1.0,1.0);
     guy.setTexture(texture_guy);
 
 
@@ -115,6 +96,7 @@ int main() {
     wall.setTexture(texture_wall);
     wall.setScale(0.3, 0.3);
     wall.setTextureRect(sf::IntRect(0, 0, 500, 500));
+    wall.setPosition(100.0,50.0);
 
     //--------
 
@@ -126,7 +108,10 @@ int main() {
 
     sf::Clock clock;
 
+
+    guy.setBounds(0, window.getSize().x, 0, window.getSize().y);
     while (window.isOpen()) {
+        sf::Time elapsed = clock.restart();
 
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -134,19 +119,18 @@ int main() {
                 window.close();
         }
 
-        if(event.type == sf::Event::KeyReleased)
-            {
-                if(event.key.code == sf::Keyboard::Space)
-                {
-                    //guy.animate(elapsed);
-                    std::cout << "Space released" << std::endl;
-                }
-            }
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        {
-          //  rectangle_inh.animate(elapsed);
-        }
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+            guy.setSpeed(100,0);
+            guy.animate(elapsed);}
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+            guy.setSpeed(-100,0);
+            guy.animate(elapsed);}
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
+            guy.setSpeed(0,-100);
+            guy.animate(elapsed);}
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+            guy.setSpeed(0,100);
+            guy.animate(elapsed);}
 
         window.clear(sf::Color::Black);
 
